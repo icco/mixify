5824a551df9eaf8ab24d23f695fc43f0f2293583
Alex Baldwin <alexbaldwin@gmail.com>
2015-05-31 16:22:28 -0700

    Basing it off Soundcloud Sets

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # Mixify
 
-Turns a list of Soundcloud URLs into a crossfaded mix.
+Turns a list of Soundcloud Set URL into a crossfaded mix.
 
 Songs must be .wavs so this helps
 
-`youtube-dl [URL] -x --audio-format wav`
+`ruby script.rb [Soundcloud Set URL]`
 
-How to run it:
-
-`sh crossfade_cat.sh 11 1.wav 2.wav auto auto`
-
-Tame the final result by turning it into an mp3
-
-`ffmpeg -i mix.wav -codec:a libmp3lame -qscale:a 2 output.mp3`
+ie.  `ruby script.rb https://soundcloud.com/alexbaldwin/sets/mixify-demo`
--- script.rb
+++ script.rb
@@ -1,14 +1,11 @@
 require 'fileutils'
 
-tracks = [
-  "https://soundcloud.com/sensualmusique/eklo-lets-go-home-fanfar-remix",
-  "https://soundcloud.com/axmod/major-lazer-dj-snake-lean-on-axmod-remix",
-  "https://soundcloud.com/tocadisco/koen-groeneveld-100-tocadisco-remix",
-  "http://hypem.com/track/28jd8/Favela+-+Gong",
-  "https://www.youtube.com/watch?v=nuHfVn_cfHU"
-]
+setlist = ARGV[0]
+p setlist
+mix_name = setlist.sub("https://soundcloud.com/","").gsub!("/","-").sub("-sets","")
+p mix_name
 date_now = Time.now.strftime("%m-%d-%Y")
-mix_directory = "mixes/#{date_now}"
+mix_directory = "mixes/#{date_now}-#{mix_name}"
 absolute_mix_directory =  Dir.pwd + "/" + mix_directory
 intro_url = "http://f.cl.ly/items/1e1P0z240a2a3n2d003w/intro.wav"
 crossfade_duration = 7
@@ -16,19 +13,14 @@ intro_location = absolute_mix_directory + "/mix.wav"
 
 FileUtils::mkdir_p mix_directory
 
-tracks.each do |track|
-  system "youtube-dl #{track} -x --audio-format wav -o '#{absolute_mix_directory}/tracks/%(id)s.%(ext)s'"
-  p track
-end
+system "youtube-dl #{setlist} -x --audio-format wav -o '#{absolute_mix_directory}/tracks/%(id)s.%(ext)s'"
 
 mix_tracks = Dir["#{absolute_mix_directory}/tracks/*.wav"]
 
 system "wget -O #{intro_location} #{intro_url}"
 
 mix_tracks.each do |track|
-  p track
   system "sh crossfade_cat.sh #{crossfade_duration} #{intro_location} #{track} auto auto #{intro_location}"
-  p track + " has been faddded"
 end
 
 system "rm -rf #{absolute_mix_directory}/tracks"
