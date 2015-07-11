2c51d151086d201c691418053862ce3abb13d648
Alex Baldwin <alexbaldwin@gmail.com>
2015-07-11 14:19:31 -0700

    Pagination and time diff fixing

--- feed-me/script.rb
+++ feed-me/script.rb
@@ -1,28 +1,35 @@
 require 'dotenv'
 require 'soundcloud'
 require 'awesome_print'
-require 'time_diff'
 Dotenv.load
 
-client = SoundCloud.new({
-  :client_id     => ENV['CLIENT_ID'],
-  :client_secret => ENV['CLIENT_SECRET'],
-  :username      => ENV['SOUNDCLOUD_USERNAME'],
-  :password      => ENV['SOUNDCLOUD_PASSWORD']
-})
+def get_ranked
+  client = SoundCloud.new({
+    :client_id     => ENV['CLIENT_ID'],
+    :client_secret => ENV['CLIENT_SECRET'],
+    :username      => ENV['SOUNDCLOUD_USERNAME'],
+    :password      => ENV['SOUNDCLOUD_PASSWORD']
+  })
 
-# See the track information returned back
-# client.get('/me/activities').collection.each do |track|
-#   ap track.origin
-# end
-
-# Hacker News Sorting http://amix.dk/blog/post/19574
-#
-# (Favorites) / (Time ago in hours +2 )^1.8
-client.get('/me/activities').collection.each do |track|
-  favorites =  track.origin['favoritings_count']
-  time_ago_in_hours = Time.diff(Time.now, Time.parse(track.created_at))[:hour]
-  score = (favorites / ((time_ago_in_hours + 2)**1.8)).ceil
-  p "#{track.origin['title']} - #{score}"
+  ranked = Hash.new
+  href = '/me/activities?limit=200'
+  while href and href != ""
+    resp = client.get(href)
+    resp.collection.each do |track|
+      if track.type == "track"
+        favorites =  track.origin['favoritings_count']
+        title = track.origin['permalink_url']
+        time_ago_in_hours = (Time.now - Time.parse(track.origin.created_at)) / (60 * 60)
+        score = favorites / (time_ago_in_hours + 2)
+        if time_ago_in_hours > 8 * 24
+          return ranked
+        elsif time_ago_in_hours > 24
+          ranked[title] = score
+        end
+      end
+    end
+    href = resp.next_href
+  end
 end
 
+ap get_ranked.sort_by {|title, score| score }.reverse.to_h
